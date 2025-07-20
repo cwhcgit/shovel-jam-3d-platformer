27df7461a62efe4653ff73cf5e27d18108fc994a
hvrca <wang.haochen.c@gmail.com>
2025-07-20 21:16:16 +0800

    add toilet, shower models, update bed channeling

--- scripts/interactables/bed.gd
+++ scripts/interactables/bed.gd
@@ -3,6 +3,7 @@ class_name Bed
 
 var is_napping: bool = false
 var player_napping: Node = null
+var original_position: Vector3
 
 func _process(delta):
 	if is_napping and is_instance_valid(player_napping):
@@ -22,8 +23,24 @@ func _start_napping(player):
 	player_napping = player
 	player.set_channeling(true)
 
+	var player_model = player.get_node("Barbarian")
+	original_position = player_model.global_position
+	if player_model:
+		# Player model faces +Z, with head towards +Y. To lie on back, rotate -90 degrees on X axis.
+		player_model.rotation.x = deg_to_rad(-90)
+		# Move model to bed's center and slightly above it to prevent clipping
+		player_model.global_position = global_position + Vector3(0, 1, 0)
+	player.animation_player.play(player.ANIM_IDLE_NAME)
+
+
 func _stop_napping():
 	is_napping = false
 	if is_instance_valid(player_napping):
+		var player_model = player_napping.get_node("Barbarian")
+		if player_model:
+			# Restore model's original rotation and local position
+			player_model.global_position = original_position
+			# player_model.rotation.x = 0
+			# player_model.position = Vector3.ZERO
 		player_napping.set_channeling(false)
 	player_napping = null
--- scripts/ui/game_over_screen.gd
+++ scripts/ui/game_over_screen.gd
@@ -8,7 +8,10 @@ func _ready():
 	AudioInstancer.transition_completed.connect(_on_audio_transition_completed)
 
 func show_screen():
-	try_again_button.disabled = true
+	var music_playing = AudioInstancer.active_player.playing
+	var playing_main_theme_intense = AudioInstancer.current_track == AudioInstancer.MusicTrack.MAIN_THEME
+	if music_playing and playing_main_theme_intense:
+		try_again_button.disabled = true
 	show()
 
 func _on_audio_transition_completed():
@@ -23,10 +26,13 @@ func _on_try_again_button_pressed():
 	ScoreTimeManager.reset_game_state()
 	MotiveManager.reset_game_state()
 	
-	# Explicitly reset music to the main theme, ensuring the lock is off
-	AudioInstancer.set_music_lock(false)
-	AudioInstancer.play_music(AudioInstancer.MusicTrack.MAIN_THEME, false)
+	_reset_music()
 	
 	# Unpause and reload the scene
 	get_tree().paused = false
 	get_tree().reload_current_scene()
+
+func _reset_music():
+	# Explicitly reset music to the main theme, ensuring the lock is off
+	AudioInstancer.set_music_lock(false)
+	AudioInstancer.play_music(AudioInstancer.MusicTrack.MAIN_THEME, false)
